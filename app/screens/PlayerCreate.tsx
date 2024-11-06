501c83ba1fcb45c3658e13e5122d0fb79bdf36ed
fluva74 <fluvatech@gmail.com>
2024-11-06 16:34:23 -0600

    create notes

--- app/screens/PlayerCreate.tsx
+++ app/screens/PlayerCreate.tsx
@@ -1,6 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import styles from '../styles';
+import { requireNativeModule } from 'expo';
 
 const PlayerCreate = () => {
   return (
@@ -11,3 +12,14 @@ const PlayerCreate = () => {
 }
 
 export default PlayerCreate
+
+
+// Player First Name: required
+// Player Last Name: required
+// Player Email: required
+// Player Password: required
+// Player Confirm Password: required
+// Player Phone Number: optional
+// Player City: required
+// Player State: required
+// PDGA#: optional
