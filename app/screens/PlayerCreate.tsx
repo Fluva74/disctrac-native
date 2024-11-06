9a82dfb18cf043e5830587fbeab0729953e92d84
fluva74 <fluvatech@gmail.com>
2024-11-06 15:29:35 -0600

    added Player Pages to tree

---
+++ app/screens/PlayerCreate.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from '../styles';
+
+const PlayerCreate = () => {
+  return (
+    <View>
+      <Text>PlayerCreate</Text>
+    </View>
+  )
+}
+
+export default PlayerCreate
